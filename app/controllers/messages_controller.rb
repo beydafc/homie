d19b9cade3171fd96052c5fa28824840f2636fb1
victoria seinhart <victoriaseinhart@gmail.com>
2024-01-09 14:03:05 -1200

    updates users

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,27 @@
+class MessagesController < ApplicationController
+
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.chatroom = @chatroom
+    @message.user = current_user
+    if @message.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render "chatrooms/show", status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @messages = Message.all
+  end
+
+  def delete
+    @message = Message.destroy
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:content)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+    resources :chatrooms, only: :show do
+      resources :messages, only: :create
+    end
+
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,4 +13,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "users", to: "pages#user_index", as: "users"
+  get "users/:id", to: "pages#user_show", as: "users/id"
+  post "chatrooms", to: "chatrooms#create"
+  post "participants", to: "participants#create"
+  get "chatroom/:id", to: "chatrooms#show"
+  post "chatroom/:id", to: "messages#create"
+
 end
