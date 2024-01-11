7f9625e431a05618bd8469418b6c79639839cb09
Luis Camilo <luiscamilov@hotmail.com>
2024-01-11 13:21:52 -0500

    aregalmos el error de conexion de javascript

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,9 +5,10 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap" # @5.3.2
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+# pin "bootstrap" # @5.3.2
+# pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "mapbox-gl" # @3.0.1
 pin "process" # @2.0.1importmap
 pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.2
-
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
